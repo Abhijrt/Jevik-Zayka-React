8584c62252bed363b9660141c1a6542b9e23aa49
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-09-10 16:53:19 +0530

    update product style - 1

--- src/components/Product/ProductItem.js
+++ src/components/Product/ProductItem.js
@@ -5,28 +5,45 @@ class ProductItem extends Component {
     const { product } = this.props;
     return (
       <div key={product._id} className="product-item-container">
-        <div className="product-image">
-          <img src={product.product_image} alt={product.title} />
-        </div>
-        <div className="product-item-detail-container">
-          <div className="product-title">{product.title}</div>
-          <div className="product-price-container">
-            <div className="product-price">
-              <i class="fa fa-inr" aria-hidden="true"></i>
-              {product.selling_price}
-              <span className="product-marked-price">
-                <i class="fa fa-inr" aria-hidden="true"></i>
-                {product.marked_price}
-              </span>
+        <div className="container">
+          <div className="product-image">
+            <img src={product.product_image} alt={product.title} />
+          </div>
+          <div className="item-detail-container">
+            <div className="title">{product.title}</div>
+            <div className="rating-quantity-container">
+              <div className="rating">
+                <div>Rating</div>
+                <div>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                  <i class="fa fa-star" aria-hidden="true"></i>
+                </div>
+              </div>
+              <div className="quantity">
+                <div>Quantity</div>
+                <select name="qty">
+                  <option value="250">250 G</option>
+                  <option value="500">500 G</option>
+                  <option value="1000">1 KG</option>
+                  <option value="1500">1.5 KG</option>
+                </select>
+              </div>
             </div>
-            <div className="product-marked-price"></div>
-            <div className="product-discount">12% off</div>
+            <div className="price-cart-container">
+              <div className="price-container">
+                <div className="price">
+                  Rs 200 <span>Rs 100</span>
+                </div>
+                <div className="discount">12% off</div>
+              </div>
+              <div className="cart">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+              </div>
+            </div>
           </div>
-          <div className="product-desc">
-            An apple a day keeps the doctor away...An apple a day keeps the
-            doctor away... An apple a day keeps the doctor away
-          </div>
-          <button className="button">Add to Cart</button>
         </div>
       </div>
     );
